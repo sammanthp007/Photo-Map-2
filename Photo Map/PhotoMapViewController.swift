b8ba227ad37a885fea24c78f23e57905a0d4d2d2
sammanthp007 <samman.thapa@bison.howard.edu>
2017-04-05 21:40:08 -0400

    add map, and set region and mapview delegate

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, MKMapViewDelegate {
     var mapView: MKMapView!
 
     @IBOutlet weak var bgMapView: MKMapView!
@@ -18,6 +18,15 @@ class PhotoMapViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        bgMapView.delegate = self
+        
+        // One degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        // San Francisco Lat, Long = latitude: 37.783333, longitude: -122.416667
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.783333, longitude: -122.416667)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        mapView.setRegion(region, animated: false)
         
     }
 
